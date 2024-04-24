a74291871bc5e548aeca219dd522c67a4742fc4a
Ayhan <augurlu@ymor.com>
2024-04-24 22:58:22 +0200

    project created

---
+++ src/main/java/com/account/opening/system/AccountOpeningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.account.opening.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOpeningSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountOpeningSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/account/opening/system/AccountOpeningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.opening.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountOpeningSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
