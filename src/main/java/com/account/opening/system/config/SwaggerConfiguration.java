cc7c8279b45255f48cd4a67f3953a36b7a51b097
Ayhan <augurlu@ymor.com>
2024-04-26 14:31:47 +0200

    added swagger config for token

---
+++ src/main/java/com/account/opening/system/config/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.account.opening.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Account Opening System API")
+                        .description("Customer Account Opening System API for a bank. This API is used to open a new account, get account overview, and register a new customer")
+                        .version("1.0").contact(new Contact().name("Ayhan Ugurlu").email("ayhanugurlu@gmail.com").url(""))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
