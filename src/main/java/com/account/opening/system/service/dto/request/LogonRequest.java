14effc9b335f629ff1a622ee22d217a4d5dd735e
Ayhan <augurlu@ymor.com>
2024-04-27 00:53:29 +0200

    formatted LogonRequest

--- src/main/java/com/account/opening/system/service/dto/request/LogonRequest.java
+++ src/main/java/com/account/opening/system/service/dto/request/LogonRequest.java
@@ -2,6 +2,6 @@ package com.account.opening.system.service.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record LogonRequest (
-        @Schema(name = "token", description = "Token getting from token endpoint", example = "token")String token){
+public record LogonRequest(
+        @Schema(name = "token", description = "Token getting from token endpoint", example = "token") String token) {
 }
