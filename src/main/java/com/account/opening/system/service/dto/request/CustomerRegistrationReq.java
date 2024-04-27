473d6a43ffe56045e9c0e19abe9160b72b980056
Ayhan <augurlu@ymor.com>
2024-04-27 23:41:05 +0200

    use proper date format for dateOfBirth

--- src/main/java/com/account/opening/system/service/dto/request/CustomerRegistrationReq.java
+++ src/main/java/com/account/opening/system/service/dto/request/CustomerRegistrationReq.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public record CustomerRegistrationReq(
         @Schema(name = "name", description = "Account owner name", example = "joe") String name,
         @Schema(name = "username", description = "Account owner username", example = "joe.dash") String username,
-        @Schema(name = "dateOfBirth", description = "date of birth", example = "2000-04-25T21:32:21.960Z") @JsonFormat(pattern = "yyyy-MM-dd") Date dateOfBirth,
+        @Schema(name = "dateOfBirth", description = "date of birth", example = "2000-04-25") @JsonFormat(pattern = "yyyy-MM-dd") Date dateOfBirth,
         AddressDTO addressDTO,
         @Schema(name = "documentId", description = "document id", example = "A124") String documentId,
         @Schema(name = "currency", description = "currency", example = "EUR")String currency,
